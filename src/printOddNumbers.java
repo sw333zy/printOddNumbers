6033de5ae73ad82561eb64e9344da21e832e1280
sw333zy@gmail.com <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-01-31 13:01:14 -0500

    Initial commit

---
+++ src/printOddNumbers.java
@@ -0,0 +1,9 @@
+public class printOddNumbers {
+    public static void main(String[] args){
+        for (int i = 0; i <= 20; i++){
+            if(i % 2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
